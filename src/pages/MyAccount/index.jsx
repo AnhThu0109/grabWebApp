f256fdcc641da5267eeb8fb336cdcd2664ea026b
Anh Thu <thuthuanh19@gmail.com>
2023-09-30 19:21:17 +0700

    test commit

--- src/pages/MyAccount/index.jsx
+++ src/pages/MyAccount/index.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const MyAccount = () => {
+  const [isAdmin, setAdmin] = useState(false);
+  const a = localStorage.getItem("isAdmin")
   return(
-    <></>
+    <>
+      <h1>Hello</h1>
+    </>
 )
 }
 
