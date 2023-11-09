cc49e5a68298c2751c3109ae9ef2bb815b8b4930
Anh Thu <thuthuanh19@gmail.com>
2023-11-09 22:52:39 +0700

    add search location in db or live

--- src/pages/subParts/Components/FormComponent.jsx
+++ src/pages/subParts/Components/FormComponent.jsx
@@ -10,53 +10,80 @@ import {
 import locale from "antd/es/date-picker/locale/en_US";
 // import dayjs from "dayjs";
 import { Option } from "antd/es/mentions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 import moment from "moment/moment";
 import API_KEY from "../../../utils/KEY";
 import calculateDistance from "../../../utils/calculateDistance";
 import axios from "axios";
-import { POST_LOCATION } from "../../../utils/API";
+import { CREATE_LOCATION, SEARCH_LOCATION } from "../../../utils/API";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export default function FormGetInfo() {
   const [form] = Form.useForm();
   const [query, setQuery] = useState("");
+  const [locations, setLocations] = useState([]);
   const [predictions, setPredictions] = useState([]);
   const [queryDes, setQueryDes] = useState("");
   const [predictionsDes, setPredictionsDes] = useState([]);
   const [pickUp, setPickUp] = useState({
     lat: 0,
-    lng: 0
+    lng: 0,
   });
   const [destination, setDestination] = useState({
     lat: 0,
-    lng: 0
-  })
+    lng: 0,
+  });
+  const [isSearchLive, setIsSearchLive] = useState(false);
   const token = localStorage.getItem("token");
 
-  const handleInputChange = (e, setQueryFunction, setPredictionsFunction, inputName) => {
+  const handleInputChange = async (e, setQueryFunction, inputName) => {
     const inputValue = e.target.value;
-    form.setFieldValue(inputName, e.target.value);
+    form.setFieldValue(inputName, inputValue);
     setQueryFunction(inputValue);
 
-    // Create a new instance of google.maps.places.AutocompleteService
-    const autocompleteService =
-      new window.google.maps.places.AutocompleteService();
+    // Search location
+    if (inputValue.trim() !== "") {
+      // Search in database first
+      const locations = await searchLocationInDatabase(inputValue);
+      setLocations(locations);
+      const predictLocations = locations.map((item) => item.locationName);
+      inputName === "PickupLocation"
+        ? setPredictions(predictLocations)
+        : setPredictionsDes(predictLocations);
 
-    // Use AutocompleteService to get predictions based on the input value
-    autocompleteService.getPlacePredictions(
-      {
-        input: inputValue,
-        componentRestrictions: { country: "VN" }, // Restrict predictions to Vietnam
-      },
-      (predictions, status) => {
-        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-          setPredictionsFunction(predictions);
-        } else {
-          setPredictionsFunction([]);
-        }
+      if (locations.length === 0) {
+        // Search live
+        const predictions = await new Promise((resolve) => {
+          const autocompleteService =
+            new window.google.maps.places.AutocompleteService();
+
+          autocompleteService.getPlacePredictions(
+            {
+              input: inputValue,
+              componentRestrictions: { country: "VN" }, // Restrict predictions to Vietnam
+            },
+            (predictions, status) => {
+              if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                setIsSearchLive(true);
+                resolve(predictions);
+              } else {
+                resolve([]);
+              }
+            }
+          );
+        });
+
+        inputName === "PickupLocation"
+          ? setPredictions(predictions)
+          : setPredictionsDes(predictions);
       }
-    );
+    } else {
+      inputName === "PickupLocation"
+        ? setPredictions([])
+        : setPredictionsDes([]);
+    }
   };
 
   // const chooseLocation = (location, setQueryFunction, setPredictionsFunction, inputName) => {
@@ -66,56 +93,94 @@ export default function FormGetInfo() {
   //   form.setFieldValue(inputName, location);
   // };
 
-  const chooseLocation = async (location, setQueryFunction, setPredictionsFunction, inputName) => {
+  const searchLocationInDatabase = async (keyword) => {
+    try {
+      const response = await fetch(`${SEARCH_LOCATION}?keyword=${keyword}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error("Network response was not ok.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const chooseLocation = async (
+    location,
+    setQueryFunction,
+    setPredictionsFunction,
+    inputName
+  ) => {
     setQueryFunction(location);
     console.log(location);
     setPredictionsFunction([]);
 
-    try {
-        const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=${API_KEY}`;
+    if (isSearchLive) {
+      try {
+        const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+          location
+        )}&key=${API_KEY}`;
         const response = await fetch(geocodingApiUrl);
         const data = await response.json();
 
         if (response.ok && data.results && data.results.length > 0) {
-            const { lat, lng } = data.results[0].geometry.location;
-            console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-            debugger;
+          const { lat, lng } = data.results[0].geometry.location;
+          console.log(`Latitude: ${lat}, Longitude: ${lng}`);
 
-            // Save location to database
-            await saveLocationToDatabase(location, lat, lng, inputName);
+          // Save location to database
+          await saveLocationToDatabase(location, lat, lng, inputName);
         } else {
-            console.log('Error fetching location details:', data.error_message);
+          console.log("Error fetching location details:", data.error_message);
         }
-    } catch (error) {
-        console.log('Error fetching location details:', error);
+      } catch (error) {
+        console.log("Error fetching location details:", error);
+      }
+    } else {
+      let locationObjectArr = locations.filter(
+        (item) => item.locationName === location
+      );
+      console.log("locationObjectArr", locationObjectArr);
     }
-};
+  };
 
-const saveLocationToDatabase = async (locationName, latitude, longitude, inputName) => {
+  const saveLocationToDatabase = async (
+    locationName,
+    latitude,
+    longitude,
+    inputName
+  ) => {
     debugger;
     const input = {
-        "locationName": locationName,
-        "latitude": latitude,
-        "longitude": longitude
+      locationName: locationName,
+      latitude: latitude,
+      longitude: longitude,
     };
 
     try {
-        const response = await axios.post(POST_LOCATION, input, {
-            headers: { 
-              "Content-Type": "application/json",
-              "Authorization": token
-            }
-        });
+      const response = await axios.post(CREATE_LOCATION, input, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
 
-        if (response.status === 200) {
-            console.log(`Location ${inputName} saved successfully.`);
-        } else {
-            console.log('Error saving location:', response.statusText);
-        }
+      if (response.status === 200) {
+        console.log(`Location ${inputName} saved successfully.`);
+      } else {
+        console.log("Error saving location:", response.statusText);
+      }
     } catch (error) {
-        console.error('Error saving location:', error);
+      console.error("Error saving location:", error);
     }
-};
+  };
 
   const disabledDate = (current) => {
     // Disable dates before today (past dates)
@@ -141,18 +206,27 @@ const saveLocationToDatabase = async (locationName, latitude, longitude, inputNa
     return {};
   };
 
-  const onFinish = async() => {
-    if (form.getFieldValue("PickupLocation") === form.getFieldValue("Destination")) {
-      message.error("Pickup Location and Destination are the same. Please choose again!");
-    } else{
+  const onFinish = async () => {
+    if (
+      form.getFieldValue("PickupLocation") === form.getFieldValue("Destination")
+    ) {
+      message.error(
+        "Pickup Location and Destination are the same. Please choose again!"
+      );
+    } else {
       debugger;
       //const distance = await calculateDistance(pickUp.lat, pickUp.lng, destination.lat, destination.lng);
-      calculateDistance(pickUp.lat, pickUp.lng, destination.lat, destination.lng)
-        .then(distance => {
-            console.log('Distance:', distance);
+      calculateDistance(
+        pickUp.lat,
+        pickUp.lng,
+        destination.lat,
+        destination.lng
+      )
+        .then((distance) => {
+          console.log("Distance:", distance);
         })
-        .catch(error => {
-            console.error('Error:', error);
+        .catch((error) => {
+          console.error("Error:", error);
         });
       message.success("Submit success!");
     }
@@ -160,6 +234,10 @@ const saveLocationToDatabase = async (locationName, latitude, longitude, inputNa
   const onFinishFailed = () => {
     message.error("Submit failed!");
   };
+
+  useEffect(() => {
+    setIsSearchLive(false);
+  }, [predictions, predictionsDes]);
 
   return (
     <Form
@@ -196,15 +274,33 @@ const saveLocationToDatabase = async (locationName, latitude, longitude, inputNa
             {/* <SearchLocationInput /> */}
             <Input
               value={query}
-              onChange={(e) => handleInputChange(e, setQuery, setPredictions, "PickupLocation")}
+              onChange={(e) => handleInputChange(e, setQuery, "PickupLocation")}
             />
             <ul className="predictions-list">
-              {predictions.map((prediction) => (
+              {predictions.map((prediction, index) => (
                 <li
-                  key={prediction.place_id}
-                  onClick={() => chooseLocation(prediction.description, setQuery, setPredictions, "PickupLocation")}
+                  key={index}
+                  onClick={() =>
+                    chooseLocation(
+                      isSearchLive ? prediction.description : prediction,
+                      setQuery,
+                      setPredictions,
+                      "PickupLocation"
+                    )
+                  }
                 >
-                  {prediction.description}
+                  {isSearchLive ? (
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      className="me-1 text-black-50"
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faClock}
+                      className="me-1 text-black-50"
+                    />
+                  )}
+                  {isSearchLive ? prediction.description : prediction}
                 </li>
               ))}
             </ul>
@@ -229,17 +325,33 @@ const saveLocationToDatabase = async (locationName, latitude, longitude, inputNa
             <Input
               type="text"
               value={queryDes}
-              onChange={(e) =>
-                handleInputChange(e, setQueryDes, setPredictionsDes, "Destination")
-              }
+              onChange={(e) => handleInputChange(e, setQueryDes, "Destination")}
             />
             <ul className="predictions-list">
-              {predictionsDes.map((prediction) => (
+              {predictionsDes.map((prediction, index) => (
                 <li
-                  key={prediction.place_id}
-                  onClick={() => chooseLocation(prediction.description, setQueryDes, setPredictionsDes, "Destination")}
+                  key={index}
+                  onClick={() =>
+                    chooseLocation(
+                      isSearchLive ? prediction.description : prediction,
+                      setQueryDes,
+                      setPredictionsDes,
+                      "Destination"
+                    )
+                  }
                 >
-                  {prediction.description}
+                  {isSearchLive ? (
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      className="me-1 text-black-50"
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={faClock}
+                      className="me-1 text-black-50"
+                    />
+                  )}
+                  {isSearchLive ? prediction.description : prediction}
                 </li>
               ))}
             </ul>
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 const REGISTER = "http://localhost:5000/v1/admin/register";
 const LOGIN = "http://localhost:5000/v1/admin/login";
-const POST_LOCATION = "http://localhost:5000/v1/location/create";
+const CREATE_LOCATION = "http://localhost:5000/v1/location/create";
+const SEARCH_LOCATION = "http://localhost:5000/v1/location/search";
 const LOGOUT = "https://user-management-backend-i06j.onrender.com/v1/auth/logout";
 const LOGINTIMES = "https://user-management-backend-i06j.onrender.com/v1/login-times/";
 
-export {REGISTER, LOGIN, POST_LOCATION, LOGOUT, LOGINTIMES};
+export {REGISTER, LOGIN, CREATE_LOCATION, SEARCH_LOCATION, LOGINTIMES};
